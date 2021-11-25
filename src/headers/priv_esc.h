32d6fbf04227ac9018f89284e5b86e3727c6cef5
yairKoskas <yairi2003@gmail.com>
2021-11-25 13:25:49 +0200

    added de-escalation options

--- src/headers/priv_esc.h
+++ src/headers/priv_esc.h
@@ -1,6 +1,17 @@
 #ifndef __PRIV_ESC_H
 #define __PRIV_ESC_H
 
+#include <linux/cred.h>
+
+struct privs {
+	int pid;
+	struct cred* orig;
+	struct task_struct* orig;
+	struct privs* next;
+}
+
 void escalate_by_pid(int pid);
+void descalate_by_pid(int pid);
+void restore_all_privileges();
 
 #endif
